Fix bug in saturation detection and add overflow detection

model/psi_fix_fir.py
@@ -63,29 +63,29 @@ class psi_fix_fir:
         :return: Output data as tuple (sat, outp) where SAT is a boolean that indicates saturation and OUTP is the
                  output data.
         """
-        #Force integer (MATLAB may pass 1.0 as float)
+        # Force integer (MATLAB may pass 1.0 as float)
         decimRate = int(decimRate)
-        #Make input fixed point
+        # Make input fixed point
         inp = PsiFixFromReal(inp, self.inFmt)
         coefs = PsiFixFromReal(coefficients, self.coefFmt)
-        #Filter and round
+        # Filter
         res = lfilter(coefs, 1, inp)
+        # Throw an error if an overflow is detected for 2 reasons:
+        # 1) It is important for the user to know about the overflow because they must fix it.
+        # 2) Not all VHDL implementations match the bit-true model when overflow occurs.
+        ovf = np.zeros(res.size)
+        ovf = np.where(res > PsiFixUpperBound(self.roundFmt), 1, ovf)
+        ovf = np.where(res < PsiFixLowerBound(self.roundFmt), 1, ovf)
+        if np.any(ovf):
+            raise ValueError("psi_fix_fir : Internal overflow. The user must set suitable formats. See documentation.")
+        # Round and truncate (wrap)
         resRnd = PsiFixResize(res, self.accuFmt, self.roundFmt, PsiFixRnd.Round)
-        #Decimate
+        # Decimate
         resDec = resRnd[::decimRate]
-        #Check saturation
+        # Check saturation
         sat = np.zeros(resDec.size)
         sat = np.where(resDec > PsiFixUpperBound(self.outFmt), 1, sat)
-        sat = np.where(resDec < PsiFixUpperBound(self.outFmt), 1, sat)
-        #output
-        outp = PsiFixResize(resDec, self.roundFmt, self.outFmt, PsiFixRnd.Trunc, PsiFixSat.Sat)#No rounding since no fractional bits must be removed
+        sat = np.where(resDec < PsiFixLowerBound(self.outFmt), 1, sat)
+        # Output
+        outp = PsiFixResize(resDec, self.roundFmt, self.outFmt, PsiFixRnd.Trunc, PsiFixSat.Sat)  # No rounding since no fractional bits must be removed.
         return (sat, outp)
-
-
-
-
-
-
-
-
-